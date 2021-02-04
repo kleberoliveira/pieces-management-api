905ddca177d73be38c0f0249cc8b8b9fedc4f6bd
Kleber Oliveira <kleber.oliveira@sharecare.com>
2021-02-03 23:20:24 -0300

    feat: put app variables

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -5,15 +6,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
   const options = new DocumentBuilder()
-    .setTitle('Pieces Management')
-    .setDescription('Pieces Managemnet')
-    .setVersion('1.0')
+    .setTitle(configService.get<string>('app.title'))
+    .setDescription(configService.get<string>('app.description'))
+    .setVersion(configService.get<string>('app.version'))
     .addBasicAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(configService.get<string>('app.port'));
 }
 bootstrap();
