b45743652fb20e04b8988a27bceee24017855d46
Kleber Oliveira <kleber.oliveira@sharecare.com>
2021-02-03 23:00:44 -0300

    feat: add env variable file

---
+++ src/validation/env.schema.ts
@@ -0,0 +1,14 @@
+import * as Joi from '@hapi/joi';
+
+export default Joi.object({
+  APP_TITLE: Joi.string().required(),
+  APP_DESCRIPTION: Joi.string().required(),
+  APP_VERSION: Joi.string().default('1.0'),
+  APP_PORT: Joi.number().default(3000),
+  DATABASE_HOST: Joi.string().default('http://127.0.0.1'),
+  DATABASE_PORT: Joi.number().default(27017),
+  DATABASE_USER: Joi.string().default('root'),
+  DATABASE_PASSWORD: Joi.string().default('root'),
+  DATABASE_NAME: Joi.string().default('pieces-management'),
+  DATABASE_TZ: Joi.string().default('Brazil/East'),
+});
